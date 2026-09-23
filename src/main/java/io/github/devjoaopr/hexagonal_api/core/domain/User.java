b4bca11e3df3e43d5bb06ed79c5fd979f7831c00
devjoaopr <joaoprsantos1@outlook.com>
2026-09-22 21:36:31 -0300

    starting the project adjustment

---
+++ src/main/java/io/github/devjoaopr/hexagonal_api/core/domain/User.java
@@ -0,0 +1,10 @@
+package io.github.devjoaopr.hexagonal_api.core.domain;
+
+import java.util.UUID;
+
+public class User {
+    UUID id;
+    String email;
+    String password;
+    Boolean admin;
+}
