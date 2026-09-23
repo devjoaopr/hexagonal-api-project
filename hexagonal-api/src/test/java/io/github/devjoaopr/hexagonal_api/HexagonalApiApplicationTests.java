01d52ac14242b7144ace10eb8b6e69a4d39eaeff
devjoaopr <joaoprsantos1@outlook.com>
2026-09-22 21:16:24 -0300

    adding first changes

---
+++ hexagonal-api/src/test/java/io/github/devjoaopr/hexagonal_api/HexagonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devjoaopr.hexagonal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
